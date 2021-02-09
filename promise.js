6f8157e685deeb03fd6afcfe0e17a96041d92b5e
SSB <abdullahferdus@gmail.com>
2021-02-09 21:30:54 +0600

    Latest

--- promise.js
+++ promise.js
@@ -2,15 +2,17 @@
 // resolve() ---> then()
 // reject() ---> catch()
 
-function prom(complete){
+function prom(complete) {
+
     return new Promise(function(resolve, reject){
-        if(complete){
-            resolve('I am successfull');
-        } else {
-            reject('I am failed');
+        if(complete) {
+            resolve('I am successful.')
+        }else {
+            reject('I am failed.')
         }
     });
 }
 
-
 console.log(prom(true))
+
+
